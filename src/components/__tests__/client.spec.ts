e5de9c3ecc9c1658b53ac731108391e484336153
Chelsea Lura <mail@chelsealura.com>
2024-10-25 16:57:34 -0500

    add empty test

--- src/components/__tests__/client.spec.ts
+++ src/components/__tests__/client.spec.ts
@@ -1,7 +1,11 @@
-import { describe } from 'vitest'
+import { describe, expect, it } from 'vitest'
 
 describe('client', () => {
   describe('getDrinkRecipes', () => {
+    // I added this empty test or else the suite fails because there aren't any tests yet
+    it('runs', () => {
+      expect(true).toEqual(true)
+    })
     // Test URL is grabbed properly from env file/etc, that the subdirectory or query pramaters for a specific call are correct
     // confirm correct config object
     // confirm error response (in this case alerts, but a better way to handle them is in the client file)
